6fa95e42818103c77b2f18052d58904ba051cd8d
ArtakSaakyan <artak.saakyan.99@mail.ru>
2022-12-28 19:11:52 +0500

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let number = Math.abs(n)
+  let numberReverse = Number(number.toString().split('').reverse().join(''))
+  return numberReverse
 }
+
+// function reverse (n) {
+//   let number = Math.abs(n)
+//   let numberReverse = Number(number.toString().split('').reverse().join(''))
+//   return numberReverse
+// }
+
+// console.log(reverse(123))
+// console.log(reverse(-123))
